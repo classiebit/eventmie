3525ba6c2bd7a6ddb2b4934690ae0e43ae32d15b
Deepak Panwar <dp.classiebit@gmail.com>
2019-10-08 12:05:33 +0530

    Update Eventmie Seeder

--- publishable/database/seeds/EventmieDatabaseSeeder.php
+++ publishable/database/seeds/EventmieDatabaseSeeder.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Classiebit\Eventmie\Traits\Seedable;
 
-class DatabaseSeeder extends Seeder
+class EventmieDatabaseSeeder extends Seeder
 {
+    use Seedable;
+
+    protected $seedersPath = __DIR__.'/';
+
     /**
      * Seed the application's database.
      *
@@ -11,20 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(BannersTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
-        $this->call(DataTypesTableSeeder::class);
-        $this->call(DataRowsTableSeeder::class);
-        $this->call(EventsTableSeeder::class);
-        $this->call(PagesTableSeeder::class);
-        $this->call(SettingsTableSeeder::class);
-        $this->call(MenusTableSeeder::class);
-        $this->call(MenuItemsTableSeeder::class);
-        $this->call(TranslationsTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+        $this->seed('BannersTableSeeder');
+        $this->seed('CategoriesTableSeeder');
+        $this->seed('CountriesTableSeeder');
+        $this->seed('DataTypesTableSeeder');
+        $this->seed('DataRowsTableSeeder');
+        $this->seed('EventsTableSeeder');
+        $this->seed('PagesTableSeeder');
+        $this->seed('SettingsTableSeeder');
+        $this->seed('MenusTableSeeder');
+        $this->seed('MenuItemsTableSeeder');
+        $this->seed('TranslationsTableSeeder');
+        $this->seed('PermissionsTableSeeder');
+        $this->seed('RolesTableSeeder');
+        $this->seed('PermissionRoleTableSeeder');
+        $this->seed('UsersTableSeeder');
     }
 }
